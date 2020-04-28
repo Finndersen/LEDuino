1609b7579ab39e64f20cf8103ec30a456e6a8627
finndersen <finndersen@gmail.com>
2020-04-29 01:45:16 +0930

    - More work on Point struct - Created SpatialAxis subclass of LEDAxis - Various fixes

--- LEDController.h
+++ LEDController.h
@@ -5,6 +5,7 @@
 #define  LEDController_h
 #include <Arduino.h>
 #include <FastLED.h>
+#include <cmath>
 //#include <array>
 //#include <vector>
 #ifdef DEBUG    //Macros are usually in all capital letters.
@@ -21,10 +22,8 @@
  */
 class LEDAxis {
 	public:
-//		byte id;
 		// Constructor
 		LEDAxis(
-//			byte id,                    // unqiue ID of axis (used to identify when assigning LED values)
 			unsigned int start_offset,  // Start offset of axis (relative to start of LED strip)
 			unsigned int axis_len,      // Length of LED axis
 			unsigned int strip_len,     // Full length of LED strip (to enable wrap-over)
@@ -69,7 +68,7 @@ class LEDAxis {
 				return LEDAxis(new_start_offset, axis_len, strip_len, true);
 			}
 		}
-	private:
+		
 		unsigned int start_offset, axis_len, strip_len;
 		bool reverse;
 };
@@ -121,20 +120,20 @@ class BasePattern	{
 };
 
 // Base class for patterns defined on a single linear axis
-// Generate LED value for each position along axis
+// Converts an axis positio into an LED value
 class LinearPattern: public BasePattern	{
 	public:
 		LinearPattern(	
 			unsigned int axis_len,
 			unsigned int frame_delay,					// Delay between pattern frames (in ms)
 			byte duration=DEFAULT_PATTERN_DURATION		// Duration of pattern (in s)
-			): axis_len(axis_len), BasePattern(frame_delay, duration) {}
+			): BasePattern(frame_delay, duration), axis_len(axis_len)  {}
 		
 		unsigned int axis_len;
 		
 		// Get value for LED at position 'i' along axis 
 		virtual CRGB get_led_value(unsigned int i);
-}
+};
 
 // Base class for linear pattern which uses an array of length t_axis_len to store state 
 // Used for more complex patterns that need to use detailed state from previous frame
@@ -142,7 +141,7 @@ template<unsigned int t_axis_len>
 class LinearStatePattern : public LinearPattern	{
 	public:
 		// Constructor
-		StatePattern(	
+		LinearStatePattern(	
 			unsigned int frame_delay,							// Delay between pattern frames (in ms)
 			byte duration=DEFAULT_PATTERN_DURATION		// Duration of pattern (in s)
 			): LinearPattern(t_axis_len, frame_delay, duration) {
@@ -165,68 +164,75 @@ class LinearStatePattern : public LinearPattern	{
 
 // Structure to represent a cartesian coordinate or vector 
 struct Point {
-	public:
-		float x=0.0, y=0.0, z=0.0;
-		
-		//Initialise explicitly
-		Point(float x, float y, float z): x(x), y(y), z(z)	{};
-		// Initialise from array
-		Point(float* arr): x(arr[0]), y{arr[1]), z(arr[2])	{};
-		
-		// Vector Addition and subtraction
-		Point& operator+=(const Point &RHS) { x += RHS.x; y += RHS.y; z += RHS.z; return *this; };
-		Point& operator-=(const Point &RHS) { x -= RHS.x; y -= RHS.y; z -= RHS.z; return *this; };
-		
-		Point operator+(const Point &RHS) { return Point(*this) += RHS; };
-		Point operator-(const Point &RHS) { return Point(*this) -= RHS; };
-		
-		// Scalar addition and subtraction
-		Point& operator+=(const double &RHS) { x += RHS; y += RHS; z += RHS; return *this; };
-		Point& operator-=(const double &RHS) { x -= RHS; y -= RHS; z -= RHS; return *this; };
-
-		Point operator+(const double &RHS) { return Point(*this) += RHS; };
-		Point operator-(const double &RHS) { return Point(*this) -= RHS; };
-		
-		// Scalar product and division
-		Point& operator*=(const double &RHS) { x *= RHS; y *= RHS; z *= RHS; return *this; };
-		Point& operator/=(const double &RHS) { x /= RHS; y /= RHS; z /= RHS; return *this; };
-
-		Point operator*(const double &RHS) { return Point(*this) *= RHS; };
-		Point operator/(const double &RHS) { return Point(*this) /= RHS; };
-		
-		// Euclidean norm
-		double norm() { 
-			return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2)); 
-		};
+	float x=0.0, y=0.0, z=0.0;
 	
+	//Initialise explicitly
+	Point(float x, float y, float z): x(x), y(y), z(z)	{};
+	// Initialise from array
+	Point(float* arr): x(arr[0]), y(arr[1]), z(arr[2])	{};
+	
+	// Vector Addition and subtraction
+	Point& operator+=(const Point &RHS) { x += RHS.x; y += RHS.y; z += RHS.z; return *this; };
+	Point& operator-=(const Point &RHS) { x -= RHS.x; y -= RHS.y; z -= RHS.z; return *this; };
+	
+	Point operator+(const Point &RHS) { return Point(*this) += RHS; };
+	Point operator-(const Point &RHS) { return Point(*this) -= RHS; };
+	
+	// Scalar addition and subtraction
+	Point& operator+=(const double &RHS) { x += RHS; y += RHS; z += RHS; return *this; };
+	Point& operator-=(const double &RHS) { x -= RHS; y -= RHS; z -= RHS; return *this; };
+
+	Point operator+(const double &RHS) { return Point(*this) += RHS; };
+	Point operator-(const double &RHS) { return Point(*this) -= RHS; };
+	
+	// Scalar product and division
+	Point& operator*=(const double &RHS) { x *= RHS; y *= RHS; z *= RHS; return *this; };
+	Point& operator/=(const double &RHS) { x /= RHS; y /= RHS; z /= RHS; return *this; };
+
+	Point operator*(const double &RHS) { return Point(*this) *= RHS; };
+	Point operator/(const double &RHS) { return Point(*this) /= RHS; };
+	
+	// Euclidean norm
+	double norm() { 
+		return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2)); 
+	};
+	
+	// Distance to other point
+	double distance(const Point& other)	{
+		return std::sqrt(std::pow(other.x-x, 2) + std::pow(other.y-y, 2) + std::pow(other.z-z, 2)); 
+	}
 };
 
+// Pattern defined in 3D space. Converts a 3D coordinate into an LED value
+// The pattern occupies a 3D space constrained by attribute 'bounds'. 
+// 
 class SpatialPattern : public BasePattern {
 	public:
 		SpatialPattern(	
-			unsigned int axis_len,
-			unsigned int frame_delay,					// Delay between pattern frames (in ms)
-			byte duration=DEFAULT_PATTERN_DURATION		// Duration of pattern (in s)
-			): axis_len(axis_len), BasePattern(frame_delay, duration) {}
+			Point bounds,							// Point vector defining maximum magnitude of pattern space in x, y and z directions
+			unsigned int frame_delay,				// Delay between pattern frames (in ms)
+			byte duration=DEFAULT_PATTERN_DURATION	// Duration of pattern (in s)
+			): BasePattern(frame_delay, duration), bounds(bounds) {}
 		
-		unsigned int axis_len;
+		Point& bounds;
 		
-		// Get value for LED at position 'i' along axis 
-		virtual CRGB get_led_value(unsigned int i);
+		// Get value for LED at point coordinate. Point will be within range (+/-bounds.x, +/-bounds.y, +/-bounds.z)
+		virtual CRGB get_led_value(Point point);
 
+};
 
 // Base class for defining a mapping of a pattern to some kind of spatial configuration of LEDS
 // E.g. a linear strip (single axis) or 3D spatial array of LEDs composed of multiple axes
 class BasePatternMapping {
 	public:
 		// Constructor
-		BasePatternMapping(): {}
+		//BasePatternMapping() {}
 		// Proxy methods which route to associated pattern method
 		virtual void init();
 		virtual bool expired();
 		virtual bool frameReady();
 		// Excute new frame of pattern and map results to LED array
-		virtual void newFrame(CRGB* leds);	
+		virtual void newFrame(CRGB* leds, byte sound_level);	
 		
 };
 
@@ -239,7 +245,7 @@ class LinearPatternMapping: public BasePatternMapping {
 			LinearPattern* pattern,   	// Pointer to LinearPattern object
 			LEDAxis* led_axes,			// Pointer to Array of LEDAxis to map pattern to
 			byte num_axes				// Number of axes (length of led_axes)
-		): led_axes(led_axes), num_axes(num_axes), pattern(pattern), BasePatternMapping() 	{}
+		): pattern(pattern), led_axes(led_axes), num_axes(num_axes)	{}
 		
 		// Proxy methods which route to associated pattern method
 		void init()	{
@@ -253,7 +259,7 @@ class LinearPatternMapping: public BasePatternMapping {
 		}
 		
 		// Excute new frame of pattern and map results to LED array
-		void newFrame(CRGB* leds)	{
+		void newFrame(CRGB* leds, byte sound_level)	{
 			CRGB led_val;
 			pattern->newFrame(sound_level);
 			// Get pattern LED values and apply to axes
@@ -269,7 +275,6 @@ class LinearPatternMapping: public BasePatternMapping {
 
 			}
 		}
-		
 
 	private:
 		LinearPattern* pattern;
@@ -278,15 +283,46 @@ class LinearPatternMapping: public BasePatternMapping {
 
 };
 
+// Class to define spatial positioning of an axis for use with a SpatialPatternMapping
+// Generally want to define axis positions such that the coordinate origin is at the physical centre of your 
+class SpatialAxis : public LEDAxis {
+	public:
+		// Constructor for initialising spatialaxis from scratch
+		SpatialAxis(
+			unsigned int start_offset,  // Start offset of axis (relative to start of LED strip)
+			unsigned int axis_len,      // Length of LED axis
+			unsigned int strip_len,     // Full length of LED strip (to enable wrap-over)			
+			Point start_pos, 	// Start position of axis in 3D space
+			Point direction,		// Vector representing direction of axis in 3D space
+			bool reverse=false          // Whether axis is reversed (LED strip ID decreases with increasing axis value)
+		): LEDAxis(start_offset, axis_len, strip_len, reverse), start_pos(start_pos), direction(direction/direction.norm())   {}
+		
+		// Constructor for initialising from existing LEDAxis
+		SpatialAxis(
+			const LEDAxis existing_axis, 	// Pre-defined LED Axis object
+			Point start_pos, 		// Start position of axis in 3D space
+			Point direction			// Vector representing direction of axis in 3D space
+		): LEDAxis(existing_axis.start_offset, existing_axis.axis_len, existing_axis.strip_len, existing_axis.reverse), start_pos(start_pos), direction(direction/direction.norm())  {}
+		
+		// Get spatial position of an LED on the axis 
+		Point get_spatial_position(unsigned int axis_pos)	{
+			return start_pos + direction*axis_pos;
+		}
+		
+	protected:
+		Point start_pos;
+		Point direction;
+};
+
 // Class for defininig mapping configuration of 3DPattern to set of axes with spatial positioning
 class SpatialPatternMapping: public BasePatternMapping {
 	public:
 		// Constructor
 		SpatialPatternMapping(
-			LinearPattern* pattern,   	// Pointer to LinearPattern object
-			LEDAxis* led_axes,			// Pointer to Array of LEDAxis to map pattern to
+			SpatialPattern* pattern,   	// Pointer to LinearPattern object
+			SpatialAxis* spatial_axes,	// Pointer to Array of SpatialAxis to map pattern to
 			byte num_axes				// Number of axes (length of led_axes)
-		): led_axes(led_axes), num_axes(num_axes), pattern(pattern), BasePatternMapping() 	{}
+		): BasePatternMapping(), pattern(pattern), spatial_axes(spatial_axes), num_axes(num_axes) 	{}
 		
 		// Proxy methods which route to associated pattern method
 		void init()	{
@@ -300,7 +336,14 @@ class SpatialPatternMapping: public BasePatternMapping {
 		}
 		
 		// Excute new frame of pattern and map results to LED array
-		void newFrame(CRGB* leds)	{
+		void newFrame(CRGB* leds, byte sound_level)	{
+			
+		}
+	private:
+		SpatialPattern* pattern;
+		SpatialAxis* spatial_axes;
+		byte num_axes;
+};
 
 // Controller object which applies pattern LED values to appropriate axes
 class LEDController {
@@ -312,7 +355,7 @@ class LEDController {
 			BasePatternMapping* pattern_mappings,				// Pointer to Array of pattern to axes mapping configurations to run
 			byte num_patterns,									// Number of pattern configurations (length of pattern_mappings)
 			bool randomize=true									// Whether to randomize pattern order
-			): led_axes(led_axes), num_axes(num_axes), leds(leds), num_leds(num_leds), pattern_mappings(pattern_mappings), num_patterns(num_patterns), randomize(randomize) {
+			):  leds(leds), num_leds(num_leds), pattern_mappings(pattern_mappings), num_patterns(num_patterns), randomize(randomize) {
 			// Initialise as max for immediate overflow to 0 at start
 			current_mapping_id = num_patterns;
 			// Set initial pattern
@@ -320,7 +363,7 @@ class LEDController {
 		}
 		// Run pattern newFrame() if ready, set new pattern if required
 		void loop(byte sound_level=0) {
-			unsigned long frame_time = millis();
+			//unsigned long frame_time = millis();
 			// Check if pattern config needs to be changed
 			if (current_mapping->expired())	{
 				setNewPatternMapping();
@@ -329,7 +372,7 @@ class LEDController {
 			if (current_mapping->frameReady())	{
 				// Run pattern frame logic
 				DPRINTLN("New Frame");
-				current_mapping->newFrame(leds);
+				current_mapping->newFrame(leds, sound_level);
 				// Show LEDs
 				FastLED.show();
 			}
